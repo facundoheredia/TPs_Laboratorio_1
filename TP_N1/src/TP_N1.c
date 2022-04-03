3af8e7dde84c7ad3df149d0bcf27089c5b97fe6d
facundo <arq.facundoheredia@gmail.com>
2022-04-02 21:08:13 -0300

    Creacion de carpeta TP_N1

---
+++ TP_N1/src/TP_N1.c
@@ -0,0 +1,70 @@
+/*
+ ==============================================================================================
+																					1C - 2022
+										TRABAJO PRACTICO N°1
+									Programación I – Laboratorio I
+
+								 Tecnicatura Superior en Programación
+											  UTN-FRA
+
+AUTORES		MARINA CARDOZO
+REVISORES	LAURA CARELLI
+
+ALUMNO		HEREDIA FACUNDO
+DIVISION	1F
+
+ ==============================================================================================
+
+ENUNCIADO
+
+Una agencia de viaje necesita calcular costos para sus vuelos de Latam y Aerolíneas Argentinas
+para ofrecerlos a sus clientes.
+Se deberá ingresar por cada vuelo los km totales y el precio total del mismo.
+El objetivo de la aplicación es mostrar las diferentes opciones de pagos a sus clientes.
+Para ello el programa iniciará y contará con un menú de opciones:
+1. Ingresar Kilómetros: ( km=x)
+2. Ingresar Precio de Vuelos: (Aerolíneas=y, Latam=z)
+- Precio vuelo Aerolíneas:
+- Precio vuelo Latam:
+3. Calcular todos los costos:
+a) Tarjeta de débito (descuento 10%)
+b) Tarjeta de crédito (interés 25%)
+c) Bitcoin (1BTC -> 4606954.55 Pesos Argentinos)
+d) Mostrar precio por km (precio unitario)
+e) Mostrar diferencia de precio ingresada (Latam - Aerolíneas)
+4. Informar Resultados
+“Latam:
+a) Precio con tarjeta de débito: r
+b) Precio con tarjeta de crédito: r
+c) Precio pagando con bitcoin : r
+d) Precio unitario: r
+Aerolíneas:
+a) Precio con tarjeta de débito: r
+b) Precio con tarjeta de crédito: r
+c) Precio pagando con bitcoin : r
+d) Precio unitario: r
+La diferencia de precio es : r “
+5. Carga forzada de datos
+6. Salir
+
+ACLARACIONES DEL FUNCIONAMIENTO
+
+El menú se tiene que ver tal cual está descrito en la consigna. Los resultados se tienen que
+mostrar con el mismo formato. Se puede realizar un submenú para ingresar los km y los
+precios.
+Los resultados se tienen que ver con el formato presentado en el menú. De la siguiente
+manera.
+
+ ==============================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+
+
+	return EXIT_SUCCESS;
+}
