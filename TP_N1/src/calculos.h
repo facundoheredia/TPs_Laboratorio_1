27b84e655d0704aea61bee8ec9f396c4bac00e44
facundo <arq.facundoheredia@gmail.com>
2022-04-12 20:59:24 -0300

    Modificacion documentacion en Doxygen en los .h

--- TP_N1/src/calculos.h
+++ TP_N1/src/calculos.h
@@ -7,18 +7,40 @@
 #define CALCULOS_H_
 
 //	> FUNCION DE CALCULO DE DESCUENTO O INTERES
+/// @brief Calcula un descuento o Interes (segun el parametro auxiliar pasado) con el precio pasado.
+///
+/// @param precio 		-> es el precio para calcular
+/// @param porcentaje	-> es el valor que se usara para sacar el porcentaje
+/// @param auxiliar		-> es la variable con la que le digo a la funcion si debe hacer un descuento (1) o interes
+/// @return precioFinal	-> es el valor final con descuento o interes
 
 float calculoDescuentoOInteres (float precio, int porcentaje, int auxiliar);
 
 //	> FUNCION DIFERENCIA DE DOS NUMEROS FLOTANTES
+/// @brief Calcula haciendo una resta del numero mayor con el menor, comprobando cual de los dos es mayor antes de hacer la resta.
+///
+/// @param numeroUno	-> es el primer numero para calcular la diferencia
+/// @param numeroDos	-> es el segundo numero para calcular la diferencia
+/// @return diferencia	-> es el resultado de la diferencia entre los dos numeros
 
 float calculoDiferenciaDosNumerosFlotantes (float numeroUno, float numeroDos);
 
 //	> FUNCION DE CAMBIO DE MONEDA
+/// @brief Hace al cambio de moneda segun que parametro se pase, si el tipo de cambio o el valor de la moneda con respeco a la otra
+///
+/// @param monedaUno						-> es el valor de la moneda actual que yo quiero cambiar 	(ARG A BTC)
+/// @param tipoDeCambioMonedaDos			-> es el valor de la moneda dos respecto de la moneda uno	(BTC $1 = ARG $4606954.55 -> tipo de cambio)
+/// @param valorMonedaUnoEnMonedaDos		-> es el valor de la moneda actual respecto a la moneda dos (ARG $4606954.55 = BTC $1 -> ARG por ARG)
+/// @return monedaUnoConvertidaEnMonedaDos	-> devuelve el valor de la monedaUno cambiado a la mnnedaDos
 
 float calculoCambioDeMoneda (float monedaUno, float tipoDeCambioMonedaDos, float valorMonedaUnoEnMonedaDos);
 
 //	> FUNCION DE PRECIOS UNITARIOS
+/// @brief Divide el precio total pasado por la cantidad de unidades pasadas
+///
+/// @param dividendo	-> es el precio total
+/// @param divisor		-> es la cantidad de unidades
+/// @return resultado	> es el valor de cada unidad
 
 float calculoPrecioUnitario (float dividendo, int divisor);
 
--- TP_N1/src/funcionestpuno.c
+++ TP_N1/src/funcionestpuno.c
@@ -9,11 +9,6 @@
 
 //---------------------------------------------------------------------------------------
 //	> FUNCION DE FORZAR CARGA
-/// @brief Hardcodea los datos del punto 5 para ser calculados por las distintas funciones y ser mostrados
-///
-/// @param kilometros		-> Se estandarizan 7090 Km para ser mostrados
-/// @param precioAerolineas	-> Se estandarizan $ 162965.00 como valor del vuelo y calcular en base a ellos
-/// @param precioLatam		-> Se estandarizan $ 159339.00 como valor del vuelo y calcular en base a ellos
 
 void cargaForzada (void)
  {
@@ -50,9 +45,6 @@ void cargaForzada (void)
  }
 //---------------------------------------------------------------------------------------
 //	> FUNCION PARA MOSTRAR MENSAJE QUE EVITA SEGUIR ADELANTE
-/// @brief Funcion que emite mensaje si la bandera es verdadera para evitar que el usuario siga adelante y vuelva atras
-///
-/// @param flag	-> si la vandera pasada como parametro es 1 muestra el mensaje, si no, deja al usuario continuar
 
 void mensajeDatosForzadosIngresados (int flag)
 {
